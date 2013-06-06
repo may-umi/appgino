73801eacda7ada4a99807a4380575a33aaf4fe69
may-umi <mayumimohan@gmail.com>
2013-06-06 19:04:18 +0300

    Blank Activities and buttons
    blank Activities added for eshop loyalty and about. Button images on
    main activity change on click

--- Ginos/gen/com/wbs/ginos/R.java
+++ Ginos/gen/com/wbs/ginos/R.java
@@ -22,15 +22,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int aboutus=0x7f020000;
-        public static final int arrow=0x7f020001;
-        public static final int background=0x7f020002;
-        public static final int backgroundlogo=0x7f020003;
-        public static final int eshop=0x7f020004;
-        public static final int fail=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int loyalty=0x7f020007;
-        public static final int placeorder=0x7f020008;
-        public static final int success=0x7f020009;
+        public static final int aboutusclick=0x7f020001;
+        public static final int arrow=0x7f020002;
+        public static final int background=0x7f020003;
+        public static final int backgroundlogo=0x7f020004;
+        public static final int eshop=0x7f020005;
+        public static final int eshopclick=0x7f020006;
+        public static final int fail=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int loyalty=0x7f020009;
+        public static final int loyaltyclick=0x7f02000a;
+        public static final int placeorder=0x7f02000b;
+        public static final int placeorderclick=0x7f02000c;
+        public static final int success=0x7f02000d;
     }
     public static final class id {
         public static final int aboutBtn=0x7f080000;
@@ -56,23 +60,33 @@ public final class R {
         public static final int xlargeprice=0x7f080013;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int all_categories=0x7f030001;
-        public static final int all_items=0x7f030002;
-        public static final int list_item_categories=0x7f030003;
-        public static final int list_selected_items=0x7f030004;
-        public static final int single_item=0x7f030005;
-        public static final int single_item_layout=0x7f030006;
+        public static final int about_activity=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int all_categories=0x7f030002;
+        public static final int all_items=0x7f030003;
+        public static final int eshop_activity=0x7f030004;
+        public static final int list_item_categories=0x7f030005;
+        public static final int list_selected_items=0x7f030006;
+        public static final int loyalty_activity=0x7f030007;
+        public static final int single_item=0x7f030008;
+        public static final int single_item_layout=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int about=0x7f070000;
+        public static final int eshop=0x7f070001;
+        public static final int loyalty=0x7f070002;
+        public static final int main=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050004;
         /**  Menu Strings 
          */
         public static final int selectitem=0x7f050002;
+        public static final int title_activity_about=0x7f050006;
+        public static final int title_activity_eshop=0x7f050003;
+        public static final int title_activity_loyalty=0x7f050005;
     }
     public static final class style {
         /** 
